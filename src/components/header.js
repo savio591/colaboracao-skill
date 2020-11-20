1c0a5a7f8508ef5e5e826e309c81cfa7bacd31df
savio591 <savio591@hotmail.com>
2020-11-20 00:33:20 +0000

    Logo added

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Logo from '../assets/logo.svg'
 
-export default function Header({ children }) {
+
+function Header() {
     return (
         <header>
+            <h1>TESTEEE</h1>
+            <img src={Logo}></img>
             {children}
         </header>
     );
 }
+
+export default Header
